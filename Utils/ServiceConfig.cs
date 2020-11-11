cd32cfedcc5a07f8e28029c2ea7e4223d3512146
Kirill Martynov <Deimos.km@gmail.com>
2020-11-11 20:24:53 +0500

    Create Shell to Laba 2

---
+++ Utils/ServiceConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SecurityDashboard.Utils;
+
+namespace SecurityDashboard.Utils
+{
+	class ServiceConfig
+	{
+		public static void Initialization()
+		{
+			Service.CreateLog = () => Debug.Create();
+			Service.CreateExeptionHandler = () => ExceptionHandler.Create();
+
+		}
+	}
+}
